778a1ee7f08c187e8e9b1d1a9326ab882234f19f
name <juanfdantas@gmail.com>
2018-09-05 00:04:33 -0300

    Added Animal class

--- src/models/Animal.java
+++ src/models/Animal.java
@@ -1,4 +1,48 @@
 package models;
 
 public class Animal {
+    public enum  Sex{
+        M, F, ND;
+    }
+
+    private Client client;
+    private String name;
+    private Sex sex;
+    private int age;
+
+    public Animal(Client client, String name, Sex sex, int age) {
+        this.client = client;
+        this.client.addAnimal(this);
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }

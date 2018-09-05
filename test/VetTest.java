a8645cdc70663ce0053c78bd73c31ba76df1ad45
Victor Sales <salesrvictor@gmail.com>
2018-09-05 10:35:18 -0300

    fixed tratment assertion
    Signed-off-by: salesvictor <salesrvictor@gmail.com>

--- test/VetTest.java
+++ test/VetTest.java
@@ -12,6 +12,7 @@ public class VetTest {
     private Client victor;
     private Animal nick;
     private Date date;
+    private Treatment t;
 
     @Before
     public void setUp() throws Exception {
@@ -19,6 +20,7 @@ public class VetTest {
         victor = new Client("Victor", 21, Client.Sex.M, "salesrvictor@gmail.com");
         nick = new Animal(victor, "nick", Animal.Sex.M, 2);
         date = new Date(2018,8,21);
+        t = new Treatment(nick);
     }
 
     @Test
@@ -30,12 +32,12 @@ public class VetTest {
     public void testScheduleConsult() {
         assertEquals(0, juan.getConsults().size());
 
-        juan.scheduleConsult(nick, new Treatment(nick), date);
+        juan.scheduleConsult(nick, t, date);
         assertEquals(1, juan.getConsults().size());
 
         Consult c = juan.getConsult(date);
         assertNull(c.getSymptoms());
-        assertNull(c.getTreatment());
+        assertEquals(t, c.getTreatment());
         assertEquals(date, c.getDate());
         assertNull(c.getDiagnosis());
     }
